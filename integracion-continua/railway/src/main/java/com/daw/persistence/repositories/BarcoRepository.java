43b95c439da2d91da68805a4f029a4abdcbd53aa
mgonpor <miguelgp2005@gmail.com>
2026-02-17 13:31:05 +0100

    cambio de repo

--- integracion-continua/railway/src/main/java/com/daw/persistence/repositories/BarcoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daw.persistence.repositories;
-
-import com.daw.persistence.entities.Barco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarcoRepository extends JpaRepository<Barco, Integer> {
-}
--- integracion-continua/railway/src/main/java/com/daw/persistence/repositories/PuertoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daw.persistence.repositories;
-
-import com.daw.persistence.entities.Puerto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PuertoRepository extends JpaRepository<Puerto, Integer> {
-}
